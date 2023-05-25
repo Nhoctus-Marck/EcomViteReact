3b939ab2c3918ee2f78c8cb6d7ab46d66f0d8310
Nhoctus-Mark <marcoscarbajales96@gmail.com>
2023-05-25 19:57:12 -0300

    RouterProvider

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from "../assets/logo.png"
-
+import { Link } from 'react-router-dom'
 const header = () => {
   return (
     <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
         <div className="flex items-center h-full">
             <div className="h-12">
-                <img src={logo} className="w-20" alt='' />
+                <Link to={""}>
+                    <img src={logo} className="w-20" alt='' />
+                </Link>              
             </div>
         </div>
     </header>
