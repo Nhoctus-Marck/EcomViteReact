146811917a34b1803bf8fc9f4c345f1eb768516e
Nhoctus-Mark <marcoscarbajales96@gmail.com>
2023-05-25 22:00:37 -0300

    Link to about,contact,home,menu component + Header edited (Links + cart + login buttons)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Menu from "./pages/Menu"
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
-
+      <Route index element={<Home/>}/>
+      <Route path='menu' element={<Menu/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
     </Route >
   )
 )
