b7cdcdb3946acb2229b18f7496af85feda0c80fb
Nhoctus-Mark <marcoscarbajales96@gmail.com>
2023-06-05 16:25:38 -0300

    User creation and saving in mongodb [Front , Backend]

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dontenv = require("dotenv").config()
 
 const app = express()
 app.use(cors())
@@ -8,11 +9,56 @@ app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
 
+//mongodb connection
+// console.log(process.env.MONGODB_URL)
+mongoose.set('strictQuery',false)
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>console.log("Connect to DataBase"))
+.catch((err)=>console.log(err))
+
+//Schema
+
+const userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email:{
+        type : String,
+        unique: true
+    },
+    password: String,
+    confirmPassword: String,
+    image: String,
+})
+// model
+
+const userModel = mongoose.model("user",userSchema)
+
+
+//API
+
 app.get("/",(req,res)=>{
     res.send("Server is running")
 })
 app.post("/signup", async (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
+    const { email } = req.body;
+    const data = await userModel.findOne({email : email})
+  if(data){
+    res.send({ message: "Email id is already register", alert: false });
+  } else {
+    const { firstName,lastName,email, password ,image} = req.body;
+    const user = await userModel.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        image,
+        });
+    res.status(201).json({
+        message: "Account Created successfully",
+        firstName,
+      });
+  }
 })
 
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
